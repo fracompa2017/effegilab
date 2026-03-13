96f54268c0932a2e3aaa7215b389009bd7abd627
fracompa2017 <fracompa2017@gmail.com>
2026-03-13 19:31:02 +0100

    feat: route admin integrazioni

---
+++ src/app/(admin)/admin/integrazioni/page.tsx
@@ -0,0 +1,5 @@
+import { IntegrationManagerClient } from "@/components/admin/IntegrationManagerClient";
+
+export default function AdminIntegrationsPage() {
+  return <IntegrationManagerClient />;
+}
